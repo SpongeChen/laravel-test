24d88df950259766f13b6055f8acd796aa438f95
chenyuwei <chen_yw@126.com>
2019-07-04 12:22:41 +0800

    session

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,15 +2,18 @@
 <html>
   <head>
     <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
   <body>
 
     @include('layouts._header')
 
     <div class="container">
+      @include('shared._messages')
       @yield('content')
       @include('layouts._footer')
     </div>
   </body>
+
+  <script src="{{ mix('js/app.js') }}"></script>
 </html>
